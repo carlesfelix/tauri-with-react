4e7663e1f85651032bf54b19ce417103ff978005
Carles Fèlix Tur <carlesfelix@gmail.com>
2023-01-23 15:02:50 +0100

    feat: scaffolding

---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+import svgrPlugin from 'vite-plugin-svgr'
+import postcssImport from 'postcss-import'
+import postcssPresetEnv from 'postcss-preset-env'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  // Vite options tailored for Tauri development
+  // and only applied in `tauri dev` or `tauri build`
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true
+  },
+  css: {
+    devSourcemap: true,
+    postcss: {
+      plugins: [
+        postcssImport(),
+        postcssPresetEnv({
+          features: {
+            'nesting-rules': true
+          }
+        })
+      ]
+    }
+  },
+  // to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // Tauri supports es2021
+    target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG
+  }
+})
