12fe7b85aadbba7e3cae170690458819d1bcc923
Carles Fèlix Tur <carlesfelix@gmail.com>
2023-01-24 10:41:30 +0100

    feat: add css architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ReactElement, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import { invoke } from '@tauri-apps/api/tauri'
 import './App.css'
 
@@ -18,34 +17,18 @@ function App (): ReactElement {
   }
 
   return (
-    <div className="container">
+    <div>
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank" rel="noreferrer">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => { setName(e.currentTarget.value) }}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={greetHandler}>
-            Greet
-          </button>
-        </div>
+      <div>
+        <input
+          id="greet-input"
+          onChange={(e) => { setName(e.currentTarget.value) }}
+          placeholder="Enter a name..."
+        />
+        <button type="button" onClick={greetHandler}>
+          Greet
+        </button>
       </div>
       <p>{greetMsg}</p>
     </div>
